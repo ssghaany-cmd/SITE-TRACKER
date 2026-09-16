d32d292f4f83493abbade2face8dd9729bc79325
Muhammad sani shehu <ssghaany@gmail.com>
2026-09-16 10:14:02 +0100

    Remove type parameter from createClient instantiation

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -17,7 +17,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
   )
 }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
